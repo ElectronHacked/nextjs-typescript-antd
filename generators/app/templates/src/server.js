e82046d0ddd29a4b7337977ac0f789df68e064ca
Phil Mashiane <mashianephil@gmail.com>
2019-04-04 18:51:25 +0200

    Disabled i18next debugging

--- generators/app/templates/src/server.js
+++ generators/app/templates/src/server.js
@@ -1,60 +1,55 @@
-const express = require('express');
-const path = require('path');
-const next = require('next');
+const express = require ('express');
+const path = require ('path');
+const next = require ('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const app = next ({dev});
+const handle = app.getRequestHandler ();
 
-const i18nextMiddleware = require('i18next-express-middleware');
-const Backend = require('i18next-node-fs-backend');
-const { i18nInstance } = require('./i18n');
+const i18nextMiddleware = require ('i18next-express-middleware');
+const Backend = require ('i18next-node-fs-backend');
+const {i18nInstance} = require ('./i18n');
 
 // init i18next with serverside settings
 // using i18next-express-middleware
-i18nInstance
-  .use(Backend)
-  .use(i18nextMiddleware.LanguageDetector)
-  .init(
-    {
-      fallbackLng: 'en',
-      preload: ['en'], // preload all langages
-      ns: ['common', 'home', 'about' /* new-i18n-namespace-here */],
-      backend: {
-        loadPath: path.join(__dirname, '/static/locales/{{lng}}/{{ns}}.json'),
-        addPath: path.join(
-          __dirname,
-          '/static/locales/{{lng}}/{{ns}}.missing.json'
-        ),
-      },
-    },
-    () => {
-      // loaded translations we can bootstrap our routes
-      app.prepare().then(() => {
-        const server = express();
+i18nInstance.use (Backend).use (i18nextMiddleware.LanguageDetector).init ({
+  fallbackLng: 'en',
+  preload: ['en'], // preload all langages
+  ns: ['common', 'home', 'about' /* new-i18n-namespace-here */],
+  backend: {
+    loadPath: path.join (__dirname, '/static/locales/{{lng}}/{{ns}}.json'),
+    addPath: path.join (
+      __dirname,
+      '/static/locales/{{lng}}/{{ns}}.missing.json'
+    ),
+  },
+  debug: false,
+}, () => {
+  // loaded translations we can bootstrap our routes
+  app.prepare ().then (() => {
+    const server = express ();
 
-        // enable middleware for i18next
-        server.use(i18nextMiddleware.handle(i18nInstance));
+    // enable middleware for i18next
+    server.use (i18nextMiddleware.handle (i18nInstance));
 
-        // serve locales for client
-        server.use(
-          '/locales',
-          express.static(path.join(__dirname, '/static/locales'))
-        );
+    // serve locales for client
+    server.use (
+      '/locales',
+      express.static (path.join (__dirname, '/static/locales'))
+    );
 
-        // missing keys
-        server.post(
-          '/locales/add/:lng/:ns',
-          i18nextMiddleware.missingKeyHandler(i18nInstance)
-        );
+    // missing keys
+    server.post (
+      '/locales/add/:lng/:ns',
+      i18nextMiddleware.missingKeyHandler (i18nInstance)
+    );
 
-        // use next.js
-        server.get('*', (req, res) => handle(req, res));
+    // use next.js
+    server.get ('*', (req, res) => handle (req, res));
 
-        server.listen(3000, err => {
-          if (err) throw err;
-          console.log('> Ready on http://localhost:3000');
-        });
-      });
-    }
-  );
+    server.listen (3000, err => {
+      if (err) throw err;
+      console.log ('> Ready on http://localhost:3000');
+    });
+  });
+});
